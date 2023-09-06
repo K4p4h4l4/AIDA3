c200a915c89d051028d91fc07a3adeac07e8e3b4
K4p4h4l4 <tolavio@hotmail.com>
2023-09-06 20:20:47 +0100

    Página de registo de usuário e recuperar senha
    Página de registo de usuários e recuperar senhas

--- recuperar-senha.php
+++ recuperar-senha.php
@@ -294,35 +294,26 @@
         <div class="login__holder">
             <div class="login__container">
                 <div class="login__container-header">
-                    <h2 class="login__title">Entrar na minha conta</h2>
-                    <p>Digite o seu email e senha</p>
+                    <h2 class="login__title">Recuperar a senha</h2>
+                    <p>Digite o seu email</p>
                 </div>
                 <div class="login__data">
                     <div class="login__username-container">
                         <span class="input_icon">
                             <i class="material-icons">email</i>
                         </span>
-                        <input type="email" placeholder="Insira o seu e-mail" required class="login__user-data" id="email" autocomplete="off">
+                        <input type="email" placeholder="Insira o seu e-mail" required class="login__user-data">
                         <label for="email" class="account__label">E-mail</label>
-                    </div>
-                    <div class="login__username-container">
-                        <span class="input_icon">
-                            <i class="material-icons">lock</i>
-                        </span>
-                        <input type="password" placeholder="Insira a sua senha" required class="login__user-data" id="senha" autocomplete="off">
-                        <label for="password" class="account__label">Senha</label>
                     </div>
                 </div>
                 <div class="login__btn-container">
-                    <button class="btn__login">Login</button>
+                    <button class="btn__login">Recuperar</button>
                 </div>
                 <div class="other__options">
                     <div class="other__option">
-                        <p>Novo Cliente? <a href="register.php">Crie a sua conta</a></p>
+                        <p>Lembra da senha? <a href="login.php">Voltar ao login</a></p>
                     </div>
-                    <div class="other__option">
-                        <p>Esqueceu a senha? <a href="recuperar-senha.php">Recupere sua senha</a></p>
-                    </div>
+                    
                 </div>
             </div>
         </div>
--- register.php
+++ register.php
@@ -8,7 +8,7 @@
     <link href="https://fonts.googleapis.com/css?family=Montserrat|Nunito|PT+Serif|Roboto&display=swap" rel="stylesheet">
     <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
     <!--link href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined"-->
-    <link rel="stylesheet" href="css/login.css">
+    <link rel="stylesheet" href="css/register.css">
     <link rel="stylesheet" href="css/style.css">
     <link rel="icon" href="./img/logo/logo.JPG">
 </head>
@@ -287,47 +287,28 @@
         </div>
     </div>
     
-    <!-- *****************************\
-        secção de formulário de login
-    \******************************/-->
-    <section class="login__section">
-        <div class="login__holder">
-            <div class="login__container">
-                <div class="login__container-header">
-                    <h2 class="login__title">Entrar na minha conta</h2>
-                    <p>Digite o seu email e senha</p>
+    <!-- ***********************************\
+        secção de formulário de registro
+    \************************************/-->
+    <section class="register__container">
+        <div class="register__holder">
+            <div class="register__section">
+                <div class="register__container-header">
+                    <h2 class="register__title">Criar na minha conta</h2>
+                    <p>Preencha as informações abaixo:</p>
                 </div>
-                <div class="login__data">
-                    <div class="login__username-container">
+                <div class="register__data">
+                    <div class="register__username-container">
                         <span class="input_icon">
-                            <i class="material-icons">email</i>
+                            <i class="material-icons">account</i>
                         </span>
                         <input type="email" placeholder="Insira o seu e-mail" required class="login__user-data" id="email" autocomplete="off">
                         <label for="email" class="account__label">E-mail</label>
-                    </div>
-                    <div class="login__username-container">
-                        <span class="input_icon">
-                            <i class="material-icons">lock</i>
-                        </span>
-                        <input type="password" placeholder="Insira a sua senha" required class="login__user-data" id="senha" autocomplete="off">
-                        <label for="password" class="account__label">Senha</label>
-                    </div>
-                </div>
-                <div class="login__btn-container">
-                    <button class="btn__login">Login</button>
-                </div>
-                <div class="other__options">
-                    <div class="other__option">
-                        <p>Novo Cliente? <a href="register.php">Crie a sua conta</a></p>
-                    </div>
-                    <div class="other__option">
-                        <p>Esqueceu a senha? <a href="recuperar-senha.php">Recupere sua senha</a></p>
                     </div>
                 </div>
             </div>
         </div>
     </section>
-    
     
     <!-- *****************************\
              Footer
